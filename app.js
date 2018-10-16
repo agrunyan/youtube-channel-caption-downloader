d9a1bcd395b62f216db19f1d9881dc8b334f1426
Alex Runyan <alex@runyan.co>
2018-10-16 04:01:45 -0400

    Major progress. Need to get the sentiment analysis working properly.

--- app.js
+++ app.js
@@ -5,42 +5,16 @@ let channelVideos = require("yt-channel-videos")('AIzaSyCY7gLGo-iqtU6N2XsbKNA4mr
 let getYoutubeSubtitles = require("@joegesualdo/get-youtube-subtitles-node");
 let cacheCore = require("flat-cache");
 let cache = cacheCore.load("collection");
+let senti = require('senti');
 const EventEmitter = require("events");
 const util = require("util");
 
 /**
  * App Settings
  */
-let videoNumberLimit = 3;
+let videoNumberLimit = 5;
 // PowerfulJRE, presonusaudio, MichaelSealey, PowerfulJRE or SpectreSoundStudios
 let channelName = "MichaelSealey"; 
-
-/**
- * Event Handlers
- */
-const EventNotifier = new Notifier();
-
-function Notifier() {
-  EventEmitter.call(this);
-}
-
-/**
- * Utilities
- */
-function once(fn, context) { 
-	var result;
-
-	return function() { 
-		if(fn) {
-			result = fn.apply(context || this, arguments);
-			fn = null;
-		}
-
-		return result;
-	};
-}
-
-util.inherits(Notifier, EventEmitter);
 
 /**
  * YouTube Channel Query
@@ -62,20 +36,18 @@ function queryYoutube() {
       }
     }
 
-    // Clear the cache
-    cacheCore.clearCacheById("collection");
     // Store the watch ids
     cache.setKey("video-ids", { ids: videoList });
     // All done now
     console.log("Success! " + i + " videos added from channel." + "\n");
     // Save the new array
     cache.save( true );
-    // Bye now
-    return EventNotifier.emit("video-list-complete");
+    // Exit
+    EventNotifier.emit("video-list-complete");
 
   }, (err) => {
     // Rejected
-    return console.log(err);
+    console.log(err);
   })
 }
 
@@ -83,7 +55,7 @@ function queryYoutube() {
  * Caption Query
  */
 function captionQuery(videoListArray) {
-  console.log("Requesting captions...");
+  console.log("Requesting captions..." + "\n");
 
   let wordArray = [];
 
@@ -101,28 +73,18 @@ function captionQuery(videoListArray) {
 
       // Setup the cache
       cache.setKey("captions", { words: wordArray });
-
       // Update the cache each round of iteration
       cache.save( true );      
-  
+      // Finished collecting words
+      EventNotifier.emit("word-list-complete");
+
     }, (err) => {
       console.log("Error: " + err);
     });
   }
-
-  let wordCount = cache.getKey("captions").words.length;
-  console.log("Success! " + wordCount + " words found." + "\n");
-
-  // Finished collecting words
-  return EventNotifier.emit("word-list-complete");
 }
 
-let cleanCacheOnce = once(function() {
-  return cacheCore.clearAll();
-});
-
 async function init() {
-  cleanCacheOnce();
   return await queryYoutube();
 }
 
@@ -131,14 +93,41 @@ async function init() {
  */
 init();
 
+/**
+ * Utilities
+ */
+util.inherits(Notifier, EventEmitter);
+
+/**
+ * Event Handlers
+ */
+function Notifier() {
+  EventEmitter.call(this);
+}
+
+let logSenti = function(val) {
+  console.log(val);
+}
+
+const EventNotifier = new Notifier();
+
 // Wait for the fun
+EventNotifier.on("ready-for-analysis", () => {
+  senti(cache.getKey("captions").words, logSenti);
+});
+
 EventNotifier.on("video-list-complete", () => {
   let list = cache.getKey("video-ids");
-  return captionQuery(list);
+  captionQuery(list);
 });  
 
 EventNotifier.on("word-list-complete", () => {
-  let cap = cache.getKey("captions");
-  console.log(cap.words);
-  return cacheCore.clearCacheById("collection");
+  let captionCollection = cache.getKey("captions");
+  let wordCount = captionCollection.words.length;
+
+  console.log("So far " + wordCount + " words have been found.");
 });
+
+console.log("Time to analyze for sentiment...");
+
+return EventNotifier.emit("ready-for-analysis");
